e4c80ee4dff7feb6f963fa95b4b5af264f7493c3
nolon <nolon@gmx.net>
2020-05-26 07:17:44 +0200

    Download git repo

--- scripts/server.sh
+++ scripts/server.sh
@@ -5,7 +5,9 @@ set -e
 apt-get -y update
 apt-get -y install puppet git
 
+cd /home/vagrant
+git clone https://github.com/nolon/vagrant-samba4-ubu-dc.git
 #. /etc/profile.d/puppet-agent.sh
 
-#puppet apply "$vagrantsync"/manifests/server.pp
-#puppet apply "$vagrantsync"/manifests/ssh.pp
+puppet apply /home/vagrant/vagrant-samba4-ubu-dc/manifests/server.pp
+puppet apply /home/vagrant/vagrant-samba4-ubu-dc/manifests/ssh.pp
